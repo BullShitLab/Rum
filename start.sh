20747588bf89e99969ca8aebd310aa1e842bdf3b
Marcus H <60064620+JamieHoSzeYui@users.noreply.github.com>
2020-06-14 13:53:00 +0800

    Add device definition

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
+# Define device
+$device=rosy
+
 repo init -u repo init -u git://github.com/Evolution-X/manifest.git -b ten --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
 repo sync -j16
 git clone https://github.com/PratyakshM/device_xiaomi_rosy --depth=1 --single-branch device/xiaomi/rosy
 git clone https://github.com/PratyakshM/vendor_xiaomi_rosy --depth=1 --single-branch vendor/xiaomi
 git clone https://github.com/PratyakshM/kernel_xiaomi_rosy --depth=1 --single-branch kernel/xiaomi/rosy
 . build/envsetup.sh &&brunch rosy
+
