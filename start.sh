18fff58ae81961400fefac0e1b28e78c15135c94
Shéikh Adnan <manofuranium@gmail.com>
2020-06-14 15:47:48 +0530

    Minimal info from neofetch

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # confirm our specs
-neofetch
+neofetch --stdout
 
 #real shit
 repo init -u git://github.com/Evolution-X/manifest.git -b ten --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
