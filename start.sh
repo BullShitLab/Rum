47cc6f09ccd6d8180ca4a7676a64a202145f8fcb
Marcus H <60064620+JamieHoSzeYui@users.noreply.github.com>
2020-06-14 14:58:47 +0800

    revert

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
-# Define device
-$device=rosy
-
 repo init -u repo init -u git://github.com/Evolution-X/manifest.git -b ten --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
 repo sync -j16
 git clone https://github.com/PratyakshM/device_xiaomi_rosy --depth=1 --single-branch device/xiaomi/rosy
 git clone https://github.com/PratyakshM/vendor_xiaomi_rosy --depth=1 --single-branch vendor/xiaomi
 git clone https://github.com/PratyakshM/kernel_xiaomi_rosy --depth=1 --single-branch kernel/xiaomi/rosy
-. build/envsetup.sh &&brunch $device
+. build/envsetup.sh &&brunch $rosy
 
 #Post-build upload jobs
 curl -sL https://git.io/file-transfer | sh
-./transfer bit out/target/product/$device/*-$device-*-UNOFFICIAL.zip
+./transfer bit out/target/product/$device/*-rosy-*-UNOFFICIAL.zip
