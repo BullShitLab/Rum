108172c42a6686716e31a524f93b1cae9697b041
Marcus H <60064620+JamieHoSzeYui@users.noreply.github.com>
2020-06-14 15:07:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-repo init -u repo init -u git://github.com/Evolution-X/manifest.git -b ten --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
+repo init -u git://github.com/Evolution-X/manifest.git -b ten --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
 repo sync -j16
 git clone https://github.com/PratyakshM/device_xiaomi_rosy --depth=1 --single-branch device/xiaomi/rosy
 git clone https://github.com/PratyakshM/vendor_xiaomi_rosy --depth=1 --single-branch vendor/xiaomi
